99d0828a8e7b434225f38166a5b97e0312e9a1a2
NahlSyareza <availablearner@protonmail.com>
2024-12-09 07:15:43 +0700

    IR DC Blynk Intergration

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#define BLYNK_PRINT Serial
+
+#define BLYNK_TEMPLATE_ID "TMPL6fSuUMh-m"
+#define BLYNK_TEMPLATE_NAME "Zang Zing"
+#define BLYNK_AUTH_TOKEN "iccuJhsXcjRIX20sFM75onKmGgwY_R_P"
+
+#define IR_DC_MODE_VPIN V10
+#define IR_DC_VALUE_VPIN V12
+
 #include <Arduino.h>
 #include <FreeRTOSConfig.h>
 #include <ESP32Servo.h>
+#include <BlynkSimpleEsp32.h>
 
 #define INPUT_ENTER 23
 #define INPUT_EXIT 22
@@ -15,6 +25,24 @@ BaseType_t xSetPowerTask;
 int people = 0;
 bool enter_changed = false;
 bool exit_changed = false;
+
+const char *ssid = "Wokwi-GUEST";
+const char *password = "";
+
+static int IR_DC_MODE = 0;
+static int IR_DC_VALUE = 0;
+
+BLYNK_WRITE(IR_DC_MODE_VPIN)
+{
+  IR_DC_MODE = param.asInt();
+  Serial.println("Vrombop");
+}
+
+BLYNK_WRITE(IR_DC_VALUE_VPIN)
+{
+  IR_DC_VALUE = param.asInt();
+  Serial.println("Zang zing");
+}
 
 void vSetPowerTask(void *pvParameters)
 {
@@ -47,18 +75,21 @@ void vSetPowerTask(void *pvParameters)
       exit_changed = false;
     }
 
-    float formula = ((float)people / 5) * 180;
+    int formula = ((float)people / 5) * 180;
+
+    formula = !IR_DC_MODE ? formula : IR_DC_VALUE;
+
     servo.write(formula);
 
     if (people < 5)
     {
       sprintf(buff, "People is %d (Power: %.2f)", people, formula);
-      Serial.println(buff);
+      // Serial.println(buff);
     }
     else
     {
       sprintf(buff, "ZENKAIII! People is more than equal to %d (Power: %.2f)", people, formula);
-      Serial.println(buff);
+      // Serial.println(buff);
     }
 
     vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -70,6 +101,12 @@ void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+
+  Blynk.syncVirtual(IR_DC_MODE_VPIN);
+  Blynk.syncVirtual(IR_DC_VALUE_VPIN);
+
   xSetPowerTask = xTaskCreate(vSetPowerTask, "Set Power", 4096, NULL, 0, NULL);
 
   ESP32PWM::allocateTimer(0);
@@ -85,4 +122,5 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
+  Blynk.run();
 }
