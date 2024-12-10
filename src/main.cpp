901556ec64599bd1fcce8aa1ec80a86f2a3956ae
NahlSyareza <availablearner@protonmail.com>
2024-12-10 13:36:10 +0700

    p

--- src/main.cpp
+++ src/main.cpp
@@ -1,126 +1,83 @@
-#define BLYNK_PRINT Serial
-
-#define BLYNK_TEMPLATE_ID "TMPL6fSuUMh-m"
-#define BLYNK_TEMPLATE_NAME "Zang Zing"
-#define BLYNK_AUTH_TOKEN "iccuJhsXcjRIX20sFM75onKmGgwY_R_P"
-
-#define IR_DC_MODE_VPIN V10
-#define IR_DC_VALUE_VPIN V12
-
 #include <Arduino.h>
 #include <FreeRTOSConfig.h>
-#include <ESP32Servo.h>
-#include <BlynkSimpleEsp32.h>
 
-#define INPUT_ENTER 23
-#define INPUT_EXIT 22
+#define IN1 26
+#define IN2 25
+#define EN1 27
 
-#define PHYSICAL_BUILD 21
+#define PWM_CHN 0     // Channel 0
+#define LEDC_TIMER 0  // Timer 0
+#define PWM_RES 8     // 8-bit resolution (0-255)
+#define PWM_FREQ 1000 // Frequency in Hz (5 kHz)
 
-#define OUTPUT_POWER 25
-
-Servo servo;
-BaseType_t xSetPowerTask;
-
-int people = 0;
-bool enter_changed = false;
-bool exit_changed = false;
-
-const char *ssid = "Wokwi-GUEST";
-const char *password = "";
-
-static int IR_DC_MODE = 0;
-static int IR_DC_VALUE = 0;
-
-BLYNK_WRITE(IR_DC_MODE_VPIN)
-{
-  IR_DC_MODE = param.asInt();
-  Serial.println("Vrombop");
-}
-
-BLYNK_WRITE(IR_DC_VALUE_VPIN)
-{
-  IR_DC_VALUE = param.asInt();
-  Serial.println("Zang zing");
-}
-
-void vSetPowerTask(void *pvParameters)
-{
-  char buff[255];
-  while (1)
-  {
-    if (digitalRead(INPUT_ENTER) == LOW)
-    {
-      if (!enter_changed)
-      {
-        people = people + 1 > 5 ? 5 : people += 1;
-        enter_changed = true;
-      }
-    }
-    else
-    {
-      enter_changed = false;
-    }
-
-    if (digitalRead(INPUT_EXIT) == LOW)
-    {
-      if (!exit_changed)
-      {
-        people = people - 1 < 0 ? 0 : people -= 1;
-        exit_changed = true;
-      }
-    }
-    else
-    {
-      exit_changed = false;
-    }
-
-    int formula = ((float)people / 5) * 180;
-
-    formula = !IR_DC_MODE ? formula : IR_DC_VALUE;
-
-    servo.write(formula);
-
-    if (people < 5)
-    {
-      sprintf(buff, "People is %d (Power: %.2f)", people, formula);
-      // Serial.println(buff);
-    }
-    else
-    {
-      sprintf(buff, "ZENKAIII! People is more than equal to %d (Power: %.2f)", people, formula);
-      // Serial.println(buff);
-    }
-
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-  }
-}
-
-// put function declarations here:
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  // pinMode(LEDC_PIN, OUTPUT);
 
-  Blynk.syncVirtual(IR_DC_MODE_VPIN);
-  Blynk.syncVirtual(IR_DC_VALUE_VPIN);
+  if (!ledcSetup(PWM_CHN, PWM_FREQ, PWM_RES))
+  {
+    Serial.println("LEDC setup failed!");
+    while (1)
+      ; // Halt if setup fails
+  }
 
-  xSetPowerTask = xTaskCreate(vSetPowerTask, "Set Power", 4096, NULL, 0, NULL);
+  ledcAttachPin(EN1, PWM_CHN);
 
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  servo.setPeriodHertz(50);
-  servo.attach(PHYSICAL_BUILD);
+  ledcWrite(PWM_CHN, 180);
 
-  pinMode(INPUT_ENTER, INPUT_PULLUP);
+  Serial.println("Vrombop:");
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  Blynk.run();
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // for (int duty = 0; duty <= 255; duty++)
+  // {
+  //   ledcWrite(PWM_CHN, duty); // Set duty cycle
+  //   delay(10);
+  // }
+
+  // for (int duty = 255; duty >= 0; duty--)
+  // {
+  //   ledcWrite(PWM_CHN, duty);
+  //   delay(10);
+  // }
+
+  int delay_val = 1000;
+
+  int duty = 32;
+  ledcWrite(PWM_CHN, duty);
+  Serial.println(duty);
+  delay(delay_val);
+
+  duty += 32;
+  ledcWrite(PWM_CHN, duty);
+  Serial.println(duty);
+  delay(delay_val);
+
+  duty += 32;
+  ledcWrite(PWM_CHN, duty);
+  Serial.println(duty);
+  delay(delay_val);
+
+  duty += 32;
+  ledcWrite(PWM_CHN, duty);
+  Serial.println(duty);
+  delay(delay_val);
+
+  duty += 32;
+  ledcWrite(PWM_CHN, duty);
+  Serial.println(duty);
+  delay(delay_val);
+
+  duty += 32;
+  ledcWrite(PWM_CHN, duty);
+  Serial.println(duty);
+  delay(delay_val);
 }
